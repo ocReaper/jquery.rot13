1df4be885c40dc8d481ebbc9d94a3da5d4bd145e
ocReaper <ocreaper@gmail.com>
2015-02-04 18:40:53 +0100

    add handling of malto urls in links href

--- src/jquery.rot13.js
+++ src/jquery.rot13.js
@@ -6,30 +6,38 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+(function ($) {
 
-  var rot13 = function(str) {
+  var rot13 = function (str) {
     var chars = str.split('');
-    return $.map(chars, function(char) {
+
+    return $.map(chars, function (char) {
       var charCode;
-      if(!char.match(/[A-Za-z]/)) {
+
+      if (!char.match(/[A-Za-z]/)) {
         return char;
       }
+
       charCode = char.charCodeAt(0);
-      if(charCode < 97) {
+
+      if (charCode < 97) {
         charCode = (charCode - 52) % 26 + 65;
       } else {
         charCode = (charCode - 84) % 26 + 97;
       }
+
       return String.fromCharCode(charCode);
     }).join('');
   };
 
   // Collection method
-  $.fn.rot13 = function() {
-    $(this).each(function() {
-      var html = $(this).html();
-      if(html.match(/[<>]/)) {
+  $.fn.rot13 = function () {
+
+    $(this).each(function () {
+      var $this = $(this),
+          html = $this.html();
+
+      if (html.match(/[<>]/)) {
         // Here be HTML!
         // Catch any raw text preceding an initial HTML tag
         html = html.replace(/^([^<]*)</, rot13);
@@ -41,13 +49,22 @@
         // No HTML 'round these parts
         html = rot13(html);
       }
-      return $(this).html(html);
+
+      if ($this.attr('href').match(/.*mailto.*/)) {
+        var mail = $this.attr('href').replace(/mailto:(.*?)/, "$1");
+
+        mail = rot13(mail);
+        $this.attr('href', 'mailto:' + mail);
+      }
+
+      return $this.html(html);
     });
+
     return this;
   };
 
   // Static method.
-  $.rot13 = function(str) {
+  $.rot13 = function (str) {
     return rot13(str);
   };
 
